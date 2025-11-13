6c0664146668e97e810335ebd668760a37f686b2
Sarveswaran MG <sarves@Sarveswarans-MacBook-Air.local>
2025-11-14 01:05:19 +0530

    vercel fail home1 changes

--- fenivi/src/Pages/Home1.jsx
+++ fenivi/src/Pages/Home1.jsx
@@ -1,6 +1,5 @@
 import HeroSection from "../Components/HeroSection";
 import LandingSection from "../Components/LandingSection";
-import Video from "../Components/video";
 
 const images = [
   "https://plus.unsplash.com/premium_photo-1674624682288-085eff4f98da?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -15,8 +14,6 @@ export default function Home1() {
     <div>
       <HeroSection />
       <LandingSection images={images} />
-
-
     </div>
   );
 }
